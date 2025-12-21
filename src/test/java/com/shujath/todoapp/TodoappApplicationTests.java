6daf363439901e54216f60b8bae111c699455878
mrranger939 <mohammedshujathnawaz@gmail.com>
2025-12-21 19:29:41 +0530

    initialize Spring Boot project with Maven wrapper and basic structure

---
+++ src/test/java/com/shujath/todoapp/TodoappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shujath.todoapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
